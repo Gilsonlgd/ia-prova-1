0071ed7854d105c46f2f7237b9004288bf2d3259
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-05-03 14:18:07 -0300

    🐛 fix(Q9): Fix alfabeta rl propagation tree

--- Q9/main.cpp
+++ Q9/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
   queue<int> *values = new queue<int>(
-      {20, 33, -45, 31, 24, 25, -10, 20, 40, -25, 18, -42, 24, -19, 36, 41});
+      {20, 33, -45, 31, 24, 25, -10, 20, 40, -25, 18, -42, 24, -19, 36, -41});
   Tree *tree = new Tree(5, values);
 
   // tree->minMaxPropagation();
